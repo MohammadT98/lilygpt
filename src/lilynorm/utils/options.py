ed130040fab5d8d37272f91e1b68628745a40443
Mohammad Torabi <mohamad.torabi76@gmail.com>
2025-12-24 17:26:53 +0100

    Update options.py

--- src/lilynorm/utils/options.py
+++ src/lilynorm/utils/options.py
@@ -1,11 +1,10 @@
-# D:\uniPadova\Thesis\lilynorm\src\lilynorm\utils\options.py
 from dataclasses import dataclass
 
 @dataclass
 class NormOptions:
-    keep_engraving: bool = False
-    strip_scheme_blocks: bool = True
-    strip_comments: bool = True
-    normalize_whitespace: bool = True
-    expand_relative: bool = False
-    inline_variables: bool = False
+    keep_engraving: bool = False #False
+    strip_scheme_blocks: bool = True #True
+    strip_comments: bool = True #True
+    normalize_whitespace: bool = True #True
+    expand_relative: bool = False #False
+    inline_variables: bool = False #False
