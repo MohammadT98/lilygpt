33d9844e65e25d893d7e2ff738349c93982c3643
Mohammad Torabi <mohamad.torabi76@gmail.com>
2026-01-03 20:10:12 +0100

    Delete some useless files

--- src/lilynorm/evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Evaluation stage: metrics for generated music."""
-
-from .metrics import LilyPondMetrics, evaluate_generated_files
-
-__all__ = ["LilyPondMetrics", "evaluate_generated_files"]
--- src/lilynorm/inference/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Inference stage: music generation from fine-tuned models."""
-
-from .generator import MusicGenerator, GenerationConfig
-
-__all__ = ["MusicGenerator", "GenerationConfig"]
